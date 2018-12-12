828369f1d40f7c7fa100bf4dd9371cff8e66cfb5
Mohammad Reza <aminiyan.it@gmail.com>
2018-12-12 16:56:08 +0800

    models and routes created

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiresource('products','ProductsController');
+Route::group(['prefix'=>'products'],function(){
+	Route::apiresource('{product}/reviews','ReviewsController');
+});
