4cab8487f0ff6929e7bf190cbd0122a9dbca49fc
Mohammad Reza <aminiyan.it@gmail.com>
2018-12-12 18:21:46 +0800

    database seeding

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+        'name'=> $faker->word,
+        'detail'=> $faker->paragraph,
+        'stock'=> $faker->randomDigit,
+        'price'=> $faker->numberBetween(10,1000),
+        'discount'=> $faker->numberBetween(2,30)
     ];
 });
